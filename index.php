51aaea4fd316f7007b0b20d930a6173c243a4acb
Kristopher Wilson <kristopherwilson@gmail.com>
2021-02-02 23:02:02 -0500

    LOL

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+function cors() {
+    
+    // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        // you want to allow, and if so:
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+    
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            // may also be using PUT, PATCH, HEAD etc
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+        
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    
+        exit(0);
+    }
+}
+
+cors();
+
+$extByMime = [
+    'image/jpeg' => '.jpg',
+    'image/jpg' => '.jpg',
+    'image/png' => '.png',
+];
+
+$storageDir = '/data';
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+list($header, $imageSrc) = explode(',', $data['image'], 2);
+$decodedData = base64_decode($imageSrc);
+
+$title = preg_replace("/[^A-Za-z0-9 ]/", '', $data['title']);
+$number = str_pad($data['pageNumber'], 4, '0', STR_PAD_LEFT);
+
+if (!file_exists($storageDir . '/' . $title)) {
+    mkdir($storageDir . '/' . $title);
+}
+
+file_put_contents(
+    $storageDir . '/' . $title . '/' . $number . '.png', $decodedData
+);
